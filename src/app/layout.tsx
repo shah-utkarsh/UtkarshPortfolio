85f9e936a0d31c413a922a6efcaf529aafd660f3
shah-utkarsh <shahutkarshm@gmail.com>
2025-04-29 16:13:40 -0300

    Added: Header Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from "next";
+import {Inter, Calistoga} from 'next/font/google'
+import { twMerge } from "tailwind-merge";
 import "./globals.css";
+
+const inter = Inter({ 
+  subsets:['latin'],
+  variable:"--font-sans"
+})
+
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight:["400"]
+})
 
 export const metadata: Metadata = {
   title: "My Portfolio",
@@ -13,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={twMerge(
+        inter.variable, 
+        calistoga.variable, 
+        "bg-gray-900 text-white antialiased font-sans"
+        )}>{children}</body>
     </html>
   );
 }
